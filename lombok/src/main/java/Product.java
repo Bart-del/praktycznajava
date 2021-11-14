87186e56f0f13caf3de882de38eb53a6794b1c28
Bartłomiej Wysocki <bartlomiej.wyso@gmail.com>
2021-11-14 13:07:44 +0100

    add lombok project

---
+++ lombok/src/main/java/Product.java
@@ -0,0 +1,37 @@
+public class Product {
+    private String name;
+    private int price;
+    private double discount;
+
+    public Product(String name, int price, double discount) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
+
+
---
+++ lombok/src/main/java/ProductL.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ProductL {
+    private String name;
+    private int price;
+    private double discount;
+}
